b315dfcea618e1e7513b9d629555b8c929e8b92f
suvo-gh <hazra.sghosh@gmail.com>
2024-03-06 23:00:57 +0530

    frontend

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config(); 
+
+const chats = require("./data/data");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is started successfully");
+});
+
+app.get("/api/chats", (req, res) => {
+    console.log(chats)
+  res.send(chats);
+});
+
+app.get("/api/chats/:id", (req,res) => {
+    // console.log(req.params.id)
+    const singleChat = chats.find(c => c._id === req.params.id);
+    res.send(singleChat)
+})
+
+const PORT =  process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
---
+++ frontend/src/Pages/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div>
+      HOME
+    </div>
+  )
+}
+
+export default Homepage
