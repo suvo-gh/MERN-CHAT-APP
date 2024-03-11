c971ac4e3b995191a837cdb63a8e692ed702b396
suvo-gh <hazra.sghosh@gmail.com>
2024-03-11 13:06:44 +0530

    mongo db connected

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
-dotenv.config(); 
-
+const connectDB = require("./config/db");
 const chats = require("./data/data");
 
+dotenv.config();
+
+connectDB();
 const app = express();
 
 app.get("/", (req, res) => {
@@ -11,15 +13,15 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/chats", (req, res) => {
-    console.log(chats)
+  console.log(chats);
   res.send(chats);
 });
 
-app.get("/api/chats/:id", (req,res) => {
-    const singleChat = chats.find(c => c._id === req.params.id);
-    res.send(singleChat)
-})
+app.get("/api/chats/:id", (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
 
-const PORT =  process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
